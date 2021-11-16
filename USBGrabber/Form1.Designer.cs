b5c1df6e9571de79cb41b3c945cc17c8d852912f
Vadim Guseynov <hgt166782@gmail.com>
2021-11-16 22:39:14 +0300

    Добавьте файлы проекта.

---
+++ USBGrabber/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace USBGrabber
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.listBoxUSB = new System.Windows.Forms.ListBox();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonSTOP = new System.Windows.Forms.ToolStripButton();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripButtonCount = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButtonCurrent = new System.Windows.Forms.ToolStripLabel();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxUSB
+            // 
+            this.listBoxUSB.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBoxUSB.FormattingEnabled = true;
+            this.listBoxUSB.Location = new System.Drawing.Point(0, 43);
+            this.listBoxUSB.Name = "listBoxUSB";
+            this.listBoxUSB.Size = new System.Drawing.Size(800, 407);
+            this.listBoxUSB.TabIndex = 0;
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonSTOP,
+            this.toolStripProgressBar,
+            this.toolStripButtonCount,
+            this.toolStripButtonCurrent});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // toolStripButtonSTOP
+            // 
+            this.toolStripButtonSTOP.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonSTOP.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonSTOP.Image")));
+            this.toolStripButtonSTOP.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonSTOP.Name = "toolStripButtonSTOP";
+            this.toolStripButtonSTOP.Size = new System.Drawing.Size(38, 22);
+            this.toolStripButtonSTOP.Text = "STOP";
+            this.toolStripButtonSTOP.Click += new System.EventHandler(this.toolStripButtonSTOP_Click);
+            // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Size = new System.Drawing.Size(100, 22);
+            // 
+            // toolStripButtonCount
+            // 
+            this.toolStripButtonCount.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonCount.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonCount.Image")));
+            this.toolStripButtonCount.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonCount.Name = "toolStripButtonCount";
+            this.toolStripButtonCount.Size = new System.Drawing.Size(30, 22);
+            this.toolStripButtonCount.Text = "0 / 0";
+            // 
+            // toolStripButtonCurrent
+            // 
+            this.toolStripButtonCurrent.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonCurrent.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonCurrent.Image")));
+            this.toolStripButtonCurrent.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonCurrent.Name = "toolStripButtonCurrent";
+            this.toolStripButtonCurrent.Size = new System.Drawing.Size(0, 22);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "USBGrabber";
+            this.notifyIcon.Visible = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.listBoxUSB);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "USBGrabber";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxUSB;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.ToolStripLabel toolStripButtonCount;
+        private System.Windows.Forms.ToolStripButton toolStripButtonSTOP;
+        private System.Windows.Forms.ToolStripLabel toolStripButtonCurrent;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ USBGrabber/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Management;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace USBGrabber
+{
+    public partial class MainForm : Form
+    {
+        private ManagementEventWatcher watcher = new ManagementEventWatcher();
+        private WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2");
+
+        private Thread copyThread;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SetNotifyIcon();
+            this.ShowInTaskbar = false;
+
+            watcher.EventArrived += new EventArrivedEventHandler(UsbPlugged);
+            watcher.Query = query;
+
+            watcher.Start();
+        }
+
+        private void UsbPlugged(object sender, EventArgs e)
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.DriveType == DriveType.Removable)
+                {
+                    //listBoxUSB.Items.Add(string.Format("({0}) {1}", drive.Name.Replace("\\", ""), drive.VolumeLabel));
+                    CopyData(drive);
+                }
+            }
+            watcher.WaitForNextEvent();
+        }
+
+        private void CopyData(DriveInfo drive)
+        {
+            Invoke(new Action(() =>
+            {
+                toolStripButtonCount.Text = "0 / 0";
+                toolStripProgressBar.Value = 0;
+                listBoxUSB.Items.Clear();
+                toolStripButtonCurrent.Text = "";
+            }));
+            copyThread = new Thread(() =>
+            {
+                try
+                {
+                    string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + "_dump");
+                    Directory.CreateDirectory(tempDirectory);
+
+                    string[] files = Directory.GetFiles(drive.RootDirectory.Name, "*.*", SearchOption.AllDirectories);
+                    listBoxUSB.Invoke(new Action(() =>
+                    {
+                        toolStripProgressBar.Maximum = files.Length;
+                        toolStripButtonCount.Text = $"0 / {files.Length}";
+                    }));
+
+                    int i = 1;
+                    foreach (string srcPath in files)
+                    {
+                        string name = Path.GetFileName(srcPath);
+                        File.Copy(srcPath, tempDirectory + "\\" + name, true);
+                        listBoxUSB.Invoke(new Action(() =>
+                        {
+                            listBoxUSB.Items.Add($"{i}. {name}");
+                            toolStripProgressBar.Increment(1);
+                            toolStripButtonCount.Text = $"{i} / {files.Length}";
+                            toolStripButtonCurrent.Text = name;
+                        }));
+                        i++;
+                    }
+                }
+                catch (ThreadAbortException e)
+                {
+                    return;
+                }
+                catch (Exception e)
+                {
+                    //MessageBox.Show("ERROR WHILE COPYING");
+                    //MessageBox.Show(e.Message);
+                }
+            });
+
+            copyThread.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopCopy();
+        }
+
+        private void toolStripButtonSTOP_Click(object sender, EventArgs e)
+        {
+            StopCopy();
+        }
+
+        private void StopCopy()
+        {
+            if (copyThread != null)
+            {
+                copyThread.Abort();
+            }
+        }
+
+        private void SetNotifyIcon()
+        {
+            ContextMenu iconMenu = new ContextMenu();
+            MenuItem openItem = new MenuItem("Show");
+            MenuItem closeItem = new MenuItem("Exit");
+
+            var handler = new EventHandler(OnClickIconMenuItem);
+            openItem.Click += handler;
+            closeItem.Click += handler;
+            iconMenu.MenuItems.Add(openItem);
+            iconMenu.MenuItems.Add(closeItem);
+
+            notifyIcon.Text = "USBGrabber";
+            notifyIcon.BalloonTipTitle = "USBGrabber";
+            notifyIcon.BalloonTipText = "USBGrabber";
+            notifyIcon.ContextMenu = iconMenu;
+        }
+
+        private void OnClickIconMenuItem(object sender, EventArgs e)
+        {
+            var item = sender as MenuItem;
+            string text = item.Text;
+            if (text.Equals("Exit")) {
+                StopCopy();
+                this.Close();
+            } else if (text.Equals("Show"))
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+    }
+}
